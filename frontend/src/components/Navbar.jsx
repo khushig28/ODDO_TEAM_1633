c9900cfcbfc9ca8f539bbfb8ce21a057132cced1
nishi1403 <nimopanchal1417@gmail.com>
2025-07-12 14:53:55 +0530

    change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Navbar.css";
 
-function Navbar({ user, points }) {
+function Navbar({ user = { name: "Guest" }, points = 0 }) {
   return (
     <nav className="navbar">
-      <div className="navbar-left">ClothesExchange</div>
+      <div className="navbar-left">SWAP-IT</div>
       <div className="navbar-right">
         <button className="nav-btn">Sell</button>
         <button className="nav-btn">Buy</button>
-        <div className="nav-points">Points: {points}</div>
-        <div className="nav-user">{user.name}</div>
+        <div className="nav-points">{points = 0 }🪙</div>
+        <div className="nav-user">👤 {user.name}</div>
       </div>
     </nav>
   );
